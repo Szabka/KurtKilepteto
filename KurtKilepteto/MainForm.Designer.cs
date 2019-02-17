e77db4da043a6254c57ebf6c4d76bd8e195227e6
zsolt.domozi <Swicon123>
2019-02-17 15:27:38 +0100

    - show student pictures - application working based on student rules

--- KurtKilepteto/MainForm.Designer.cs
+++ KurtKilepteto/MainForm.Designer.cs
@@ -29,8 +29,13 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBoxStudentFace = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStudentFace)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -38,34 +43,69 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34.33875F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65.66125F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 362F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 365F));
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 448);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1185, 669);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBoxStudentFace
+            // 
+            this.pictureBoxStudentFace.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBoxStudentFace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxStudentFace.Location = new System.Drawing.Point(14, 23);
+            this.pictureBoxStudentFace.Name = "pictureBoxStudentFace";
+            this.pictureBoxStudentFace.Size = new System.Drawing.Size(502, 595);
+            this.pictureBoxStudentFace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxStudentFace.TabIndex = 1;
+            this.pictureBoxStudentFace.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(434, 3);
+            this.textBox1.Location = new System.Drawing.Point(822, 3);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(357, 442);
+            this.textBox1.Size = new System.Drawing.Size(357, 640);
             this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(34, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Waiting for readers...";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBoxStudentFace);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(284, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(532, 663);
+            this.panel1.TabIndex = 3;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStudentFace)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -74,6 +114,9 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBoxStudentFace;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- KurtKilepteto/MainForm.cs
+++ KurtKilepteto/MainForm.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,7 @@ namespace KurtKilepteto
     public partial class MainForm : Form
     {
         Queue<string> cardEvents = new Queue<string>();
+        Dictionary<string, string> dict = new Dictionary<string, string>();
         public MainForm()
         {
             InitializeComponent();
@@ -21,8 +24,39 @@ namespace KurtKilepteto
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            OptimizePicturesSize();
+        }
+
+        private void OptimizePicturesSize()
+        {
+            //read all possible image path in an array
+            var lines = File.ReadAllLines("configs\\nyilvantartas.csv").Select(a => a.Split(';')[1]);
+            string currPath = (System.Environment.CurrentDirectory) + "\\configs\\";
+
+            //select and resize every image what we should
+            foreach (var line in lines)
+            {                
+                Image studImg = Image.FromFile(Path.GetFullPath(Path.Combine(currPath, line)) + ".jpg");
+
+                if (studImg.Width != 480 || studImg.Height != 640)
+                {
+                    Image resizedImage = resizeImage(studImg, new Size(480, 640));
+                    studImg.Dispose(); //otherwise we have to save with different name
+                    resizedImage.Save(Path.GetFullPath(Path.Combine(currPath, line)) + ".jpg");
+                }
+            }
+
+            MessageBox.Show("Student images are resized!");
 
         }
+
+        public  Image resizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
+        }
+
+        
+
         public void CardRead(string readerName, String cardID)
         {
             Log.Information("card read event received;" + readerName + ";" + cardID);
@@ -37,6 +71,114 @@ namespace KurtKilepteto
                 newText += "\r\n";
             }
             textBox1.Invoke(new Action(() => textBox1.Text = newText));
+
+            dict = File.ReadLines("configs\\nyilvantartas.csv").Select(line => line.Split(';')).ToDictionary(line => line[0], line => line[1]);
+
+            if (dict.ContainsKey(cardID))
+            {
+                ShowStudentData(cardID, dict[cardID]);
+            } else
+            {
+                //TODO inform user - student or card not found
+                this.pictureBoxStudentFace.BackColor = Color.Red;
+                label1.Invoke(new Action(() => label1.Text = "Student not found with this CardID! Foreign card!"));
+                textBox1.Invoke(new Action(() => textBox1.Text = "Student not found with this CardID! Foreign card!" + cardID));
+            }
+
+            
         }
+
+        private void ShowStudentData(string cardID, string studentID)
+        {
+      
+            //set culture to hungarian
+            var culture = new CultureInfo("hu-HU");
+            var day = culture.DateTimeFormat.GetDayName(DateTime.Today.AddDays(1).DayOfWeek);
+           
+            //read student's txt
+            string currPath = (System.Environment.CurrentDirectory) + "\\configs\\";
+            string[] lines = File.ReadLines(Path.GetFullPath(Path.Combine(currPath, studentID)) + ".txt", Encoding.UTF8).ToArray();
+
+            //decide if exit is allowed
+            bool exitIsValid = ExitValidation(lines, day, studentID);
+            if (exitIsValid)
+            {
+                //student exit is allowed               
+                this.panel1.BackColor = Color.Green;
+
+            } else
+            {
+                //we know this student, but can't validate the exit based on rules
+                this.panel1.BackColor = Color.Red;
+            }
+            ShowStudentPicture(dict[cardID]);
+            label1.Invoke(new Action(() => label1.Text = lines[0]));
+
+            // label1.Invoke(new Action(() => label1.Text = d.DayOfWeek.ToString()     ));
+
+        }
+
+        private bool ExitValidation(string[] lines, string day, string studentID)
+        {
+            DateTime validFrom = new DateTime( Convert.ToInt32( lines[2].Split('-')[0] ), Convert.ToInt32(lines[2].Split('-')[1]), Convert.ToInt32(lines[2].Split('-')[2]));
+            if ( DateTime.Compare(validFrom, DateTime.Now ) <=0  )
+            {
+                //validFrom is actual now, 
+
+                //search lines which are valid currently
+                for (int i = 3; i < lines.Length; i++)
+                {
+                    string hunAbrevDayName = lines[i].Split(',')[0];
+
+                    if ( ! string.IsNullOrEmpty(  hunAbrevDayName  ) )
+                    if (  (day.ToUpper().StartsWith(hunAbrevDayName.ToUpper()))  || hunAbrevDayName.Equals("*"))
+                    {
+                        //allowed interval on specific day
+                        string fullInterval = lines[i].Split(',')[1]; ;
+                        string intervalStart = fullInterval.Split('-')[0];
+                        string intervalEnd = fullInterval.Split('-')[1];
+
+                        DateTime startDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, Convert.ToInt32(intervalStart.Split(':')[0]), Convert.ToInt32(intervalStart.Split(':')[1]), 0);
+                        DateTime endDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, Convert.ToInt32(intervalEnd.Split(':')[0]), Convert.ToInt32(intervalEnd.Split(':')[1]), 0);
+
+                        //start time of exit is in the past or not
+                        if (DateTime.Compare (startDate, DateTime.Now) <=0 )
+                        {
+                            //end time of exit is in the future
+                            if (DateTime.Compare(endDate, DateTime.Now) > 0)
+                            {
+                                    //student can exit
+                                    //log on screen and in file the used rule
+                                    textBox1.Invoke(new Action(() => textBox1.Text += DateTime.Now +   " Exit is allowed! Rule: " + lines[i] + " studentID: " + studentID + Environment.NewLine));
+                                    return true;
+                            }
+                        }
+                    }
+                }
+
+
+                //all rules are checked but allowance not found
+                textBox1.Invoke(new Action(() => textBox1.Text += DateTime.Now + " Exit is not allowed to " + lines[0] + " studentID: " + studentID + Environment.NewLine));
+                return false;
+            }
+            else
+            {
+                //currently is not valid student
+                textBox1.Invoke(new Action(() => textBox1.Text = "Exit is not allowed! Student is not valid today! " ));
+                return false;
+            }
+        }
+
+        private void ShowStudentPicture(string studentID)
+        {
+
+            //TODO: combine path!
+            Image studImg = Image.FromFile("configs\\" + studentID + ".jpg");
+            this.pictureBoxStudentFace.Image = studImg;
+
+            //TODO: combined path is possible for eg: ..\ etc. 
+        }
+
+       
     }
 }
